8210243f8b8342fcd69c0148dc0de9277923a0c6
eliinasvensson <elina.svensson@iths.se>
2025-10-22 19:17:33 +0200

    ändrade lite i inputen så det såg mer cleant ut

--- Meny/visaMenyn.java
+++ Meny/visaMenyn.java
@@ -1,5 +1,4 @@
 package Meny;
-
 import java.util.Scanner;
 
 public class visaMenyn {
@@ -11,11 +10,12 @@ public class visaMenyn {
         System.out.println("Dags att välja ett av alternativen: ");
         System.out.println("1. Blandade kategorier");
         System.out.println("2. Specefika kategorier");
+        System.out.print("Skriv in 1-2: ");
 
         int val = scan.nextInt();
 
         while(val != 1 && val != 2){
-            System.out.println("Ogiltigt val! Välj igen");
+            System.out.print("Ogiltigt val! Välj igen: ");
             val = scan.nextInt();
         }
         return val;
