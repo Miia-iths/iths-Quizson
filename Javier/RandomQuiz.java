3bb2497bcdf61fafe51f664704c57726ab671332
Javier <Javier.Bernabe@iths.se>
2025-10-24 09:10:46 +0200

    renovar metod

--- Javier/RandomQuiz.java
+++ Javier/RandomQuiz.java
@@ -4,33 +4,32 @@ import java.util.ArrayList;
 
 public class RandomQuiz {
 
-        System.out.println("Prova mode Kategorier:");
-        System.out.println(quiz.selectQuestions("Kategorier"));
+    
+    public static void startQuiz() {
+        Quiz quiz = new Quiz(); 
+        ArrayList<String> frågor = quiz.selectQuestions("Random");
+
+        System.out.println("\n¡Comienza el quiz!");
+        for (String fråga : frågor) {
+            System.out.println("- " + fråga);
+        }
     }
-
-
 }
 
+
 class Quiz {
-    
+
     public ArrayList<String> selectQuestions(String mode) {
         ArrayList<String> selectedList = new ArrayList<>();
-//
+
         if (mode.equals("Random")) {
             selectedList.add("Fråga Random 1");
             selectedList.add("Fråga Random 2");
         } else if (mode.equals("Kategorier")) {
-            selectedList.add("Fråga Random 1");
-            selectedList.add("Fråga Random 2");
+            selectedList.add("Fråga Kategori 1");
+            selectedList.add("Fråga Kategori 2");
         }
+
         return selectedList;
     }
-     public static void startQuiz() {
-        ArrayList<String> frågor = selectQuestions("Random");
-
-        System.out.println("\nStartar quizet!");
-        for (String fråga : frågor) {
-            System.out.println("- " + fråga);
-        }
-     }
 }
