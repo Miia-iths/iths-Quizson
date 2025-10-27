f4f57b44bbd475288f6e261b5f2292c20462d460
Miia-iths <miia.larsson@iths.se>
2025-10-27 12:11:58 +0100

    antalRatt

--- Meny/TidOchPoang.java
+++ Meny/TidOchPoang.java
@@ -1,7 +1,7 @@
 package Meny;
 
 public class TidOchPoang {
-    public static void tidPoang (long slutTid, long startTid, long startTidFraga, long totalTidFragaMillis, int antalRätt) {
+    public static void tidPoang (long slutTid, long startTid, long startTidFraga, long totalTidFragaMillis, int antalRatt) {
         
         long totalTidMillis = slutTid - startTid;
         int totalTidSekunder = (int) (totalTidMillis / 1000);
@@ -10,11 +10,11 @@ public class TidOchPoang {
 
         int totalTidFragaSekunder = (int) (totalTidFragaMillis / 1000);
         int maxTidPerFråga = 60;
-        int totalMaxSekunder = antalRätt * maxTidPerFråga;
+        int totalMaxSekunder = antalRatt * maxTidPerFråga;
         int totalPoäng = totalMaxSekunder - totalTidFragaSekunder;
 
         System.out.println("Din totala tid blev " + totalTidMinuter + " min och " + totalTidSekunderKvar + " sekunder.");
-        System.out.println("Du svarade de " + antalRätt + " frågorna på sammanlagt " + totalTidFragaSekunder + " sekunder vilket leder till " + totalPoäng + " poäng!");
+        System.out.println("Du svarade de " + antalRatt + " frågorna på sammanlagt " + totalTidFragaSekunder + " sekunder vilket leder till " + totalPoäng + " poäng!");
 
     }
 
