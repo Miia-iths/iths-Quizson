84e8d8a62578baf390ba19d7407b03ccf0dc0a0c
Miia-iths <miia.larsson@iths.se>
2025-10-27 09:03:38 +0100

    lade till tidsaspekten i randomquiz

--- Meny/RandomQuiz1.java
+++ Meny/RandomQuiz1.java
@@ -17,8 +17,13 @@ public class RandomQuiz1 {
 
         Scanner scan = new Scanner(System.in);
         int antalRätt = 0;
+        long startTidFraga;
+        long totalTidFragaMillis = 0;
+        long svarsTid = 0;
+        long startTid = System.currentTimeMillis();
 
         for (int i = 0; i < 10; i++) {
+            startTidFraga = System.currentTimeMillis();
             Fråga f = allaFrågor.get(i);
             System.out.println("Fråga " + (i + 1) + ": ");
             f.visaFråga();
@@ -26,15 +31,36 @@ public class RandomQuiz1 {
             System.out.print("Ditt svar (1-4): ");
             int svar = scan.nextInt();
 
-            if (svar == f.rättSvarIndex) {
+            long slutTidFraga = System.currentTimeMillis();
+            svarsTid = slutTidFraga - startTidFraga;
+
+            if (svarsTid > 60000) {
+            System.out.println("Du tog för lång tid! max svarstid är 60 sekunder.");
+            } else if (svar == f.rättSvarIndex) {
                 System.out.println("Rätt svar!");
                 antalRätt++;
+                totalTidFragaMillis += svarsTid;
             } else {
                 System.out.println(f.alternativ[f.rättSvarIndex - 1]);
                 //ÄNDRA
             }
         }
+        
+        long slutTid = System.currentTimeMillis();
 
         System.out.println("Du fick " + antalRätt + " av 10 rätt!");
+
+        long totalTidMillis = slutTid - startTid;
+        int totalTidSekunder = (int) (totalTidMillis / 1000);
+        int totalTidMinuter = totalTidSekunder / 60;
+        int totalTidSekunderKvar = totalTidSekunder % 60;
+        System.out.println("Din totala tid blev " + totalTidMinuter + " min och " + totalTidSekunderKvar + " sekunder.");
+
+        int totalTidFragaSekunder = (int) (totalTidFragaMillis / 1000);
+        int maxTidPerFråga = 60;
+        int totalMaxSekunder = antalRätt * maxTidPerFråga;
+        int totalPoäng = totalMaxSekunder - totalTidFragaSekunder;
+        System.out.println("Du svarade de " + antalRätt + " frågorna på sammanlagt " + totalTidFragaSekunder + " sekunder vilket leder till " + totalPoäng + " poäng!");
+
     }
 }
