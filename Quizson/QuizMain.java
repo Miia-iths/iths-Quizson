0a62cba0767e31070dbeedcfd631e4ea8c564145
Johan0103 <Johan.Hanssin@iths.se>
2025-10-27 10:42:21 +0100

    Förfining

--- Quizson/QuizMain.java
+++ Quizson/QuizMain.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class QuizMain {
     public static void main(String[] args) {
         System.out.println();
-        System.out.println("Välkommna till Quizsons frågesport!!!");
+        System.out.println("!!! Välkommen till Quizsons frågesport !!!");
         System.out.println("Spelreglerna är enkla: ");
-        System.out.println("Du väljer om du vill ha blandade eller specefika kategorier på frågorna");
-        System.out.println("Frågorna är från början värda ett visst antal poäng, men de minskar i värde ju längre tiden går");
-        System.out.println("När tiden tar slut byter spelet automatiskt till nästa fråga");
+        System.out.println("Du väljer om du vill ha 1. Blandade eller 2. Specefika kategorier på frågorna.");
+        System.out.println("Frågorna är från början värda ett visst antal poäng, men de minskar i värde ju längre tiden går.");
+        System.out.println("När tiden tar slut byter spelet automatiskt till nästa fråga.");
  
         int val = beslutAvQuiz.beslut();
  
